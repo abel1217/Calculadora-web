7521d903d000d6d69fc268852260130721b29dde
abel1217 <abel.split@gmail.com>
2023-06-12 21:30:15 -0400

    Esta es la agenda para la tarea #3

---
+++ probando.js
@@ -0,0 +1,1 @@
+function contarSeleccionados() { var cant=0 int; if (document.getElementById('checkbox1').checked) { cant++; } if (document.getElementById('checkbox2').checked) { cant++; } if (document.getElementById('checkbox3').checked) { cant++; } if (document.getElementById('checkbox4').checked) { cant++; } alert('Conoce ' + cant + ' lenguajes'); }
